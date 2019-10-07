68b719757a8495d03f77814db6ed3f0bf49fcb6a
Gabriel Pulido <gabriel.pulidodetorres@gmail.com>
2019-10-07 15:04:30 +0200

    Fix script

--- swarm-alert.sh
+++ swarm-alert.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -ex
 python /src/swarm-alert.py  --check_interval "$CHECK_INTERVAL" \
-       --user_key "$PUSHOVER_USER_KEY" --api_token "$PUSHOVER_API_TOKEN" --whitelist "$WHITE_LIST" \
-    --blacklist "$BLACK_LIST" --msg_prefix "$MSG_PREFIX" --loglevel "$LOGGING_LEVEL"
+     --whitelist "$WHITE_LIST" --blacklist "$BLACK_LIST" --msg_prefix "$MSG_PREFIX" --loglevel "$LOGGING_LEVEL"
 ## File : swarm-alert.sh ends
