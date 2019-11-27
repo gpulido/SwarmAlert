b54911bad8a28194dca64c25e2838f5dd377db36
Gabriel Pulido de Torres <gabriel.pulidodetorres@gmail.com>
2019-11-27 09:55:26 +0100

    Keep track of which services are failing to notify new ones

--- src/swarm-alert.py
+++ src/swarm-alert.py
@@ -8,9 +8,8 @@ from utils import *
 from apprise import NotifyType
 import apprise
 
-__version__ = '0.0.4-dev'
+__version__ = '0.0.5-dev'
 __author__ = 'gpt'
-
 
 
 def monitor_swarm(docker_client, white_list, black_list):
@@ -18,12 +17,12 @@ def monitor_swarm(docker_client, white_list, black_list):
     Review the docker_client containers checking for the services on the white list excluding the
     ones that are in the blacklist
     Arguments:
-        docker_client {[type]} -- [description]
-        white_list {[type]} -- List of services to look after. If empty all services will be monitored
-        black_list {[type]} -- List of services to exclude from the monitorization
+        docker_client {[str]} -- [description]
+        white_list {[str]} -- List of services to look after. If empty all services will be monitored
+        black_list {[str]} -- List of services to exclude from the monitorization
     
     Returns:
-        str -- String with the information of the running docker list
+        {[str]} -- List of string of stopped services
     """
     logger.debug("Getting services from docker")
     services = docker_client.services.list()
@@ -35,15 +34,8 @@ def monitor_swarm(docker_client, white_list, black_list):
     services_name = [service.name for service in services]
     logger.debug(str(services_name))  
     not_running_services = [service for service in services if(len(service.tasks({'desired-state':'Running'})) == 0)]
-    logger.debug("Not running:" + str([service.name for service in not_running_services]))
-    err_msg = ""
-    if len(not_running_services) != 0:
-        err_msg = "Detected Stopped Services: \n%s\n%s" % (service_list_to_str(not_running_services), err_msg)
+    return not_running_services    
 
-    if err_msg == "":
-        return NotifyType.INFO, "No stopped services"
-    else:
-        return NotifyType.FAILURE, err_msg
 
 def configure_logger(logger_level):
     import logging
@@ -52,23 +44,44 @@ def configure_logger(logger_level):
     return logging.getLogger()
     
 
+def analyse_status(stopped_services, new_stopped):    
+    recovered_services = [s for s in stopped_services if s not in stopped_services]
+    new_failing = [s for s in new_stopped if s not in stopped_services]
+    keep_failing = [ s for s in stopped_services if s in stopped_services]
+    return (recovered_services, new_failing)
+
+
 def monitor_and_notify(docker_client, apobj):
-    logger.info("Starting monitor")
+    logger.info("Starting monitor")    
+    stopped_services = []
 
-    has_send_error_alert = False
-    while True:
-        (status, err_msg) = monitor_swarm(docker_client, white_pattern_list, black_list)
-        logger.debug(" Ouput of monitor: " + status + " " + err_msg)
-        if msg_prefix != "":    
-            err_msg = "%s\n%s" % (msg_prefix, err_msg)                
-        
-        if (status == NotifyType.INFO and has_send_error_alert) or not has_send_error_alert:           
-            logger.debug("Sending notification:" + err_msg)
-            apobj.notify(body=err_msg,
+    while True:        
+        new_stopped = monitor_swarm(docker_client, white_pattern_list, black_list)
+        (recovered, failing, old_failing) = analyse_status(stopped_services, new_stopped)
+
+        if len(recovered != 0):                        
+            recovered_msg = f'{msg_prefix} Recovered Services: \n {service_list_to_str(recovered)}'   
+            logger.debug("Sending notification:" + recovered_msg)
+            apobj.notify(body=recovered_msg,
                         title='SwarmAlert',
-                        notify_type=status )                 
-            has_send_error_alert = not has_send_error_alert
+                        notify_type=NotifyType.INFO)                              
+        
+        if len(failing != 0):                        
+            failed_msg = f'{msg_prefix} Detected New Stopped Services: \n {service_list_to_str(failing)}'   
+            logger.debug("Sending notification:" + failed_msg)
+            apobj.notify(body=failed_msg,
+                        title='SwarmAlert',
+                        notify_type=NotifyType.FAILURE)        
+        
+        #Only notify old failing services if we are already notifing changes
+        if len(old_failing) != 0 and (len(failing != 0) or len(recovered != 0)):
+            old_failed_msg = f'{msg_prefix} Already Stopped Services: \n {service_list_to_str(old_failing)}'   
+            logger.debug("Sending notification:" + old_failed_msg)
+            apobj.notify(body=old_failed_msg,
+                        title='SwarmAlert',
+                        notify_type=NotifyType.FAILURE)        
 
+        stopped_services = new_stopped
         time.sleep(check_interval)
 
 if __name__ == '__main__':
